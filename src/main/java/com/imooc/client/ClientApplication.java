e73dca4990ee6e90aca9edc5c0e2b25bbaca5dc9
章鑫 <zhangx511@chinaunicom.cn>
2019-08-15 17:33:36 +0800

    eureka_client update 20190815

--- src/main/java/com/imooc/client/ClientApplication.java
+++ src/main/java/com/imooc/client/ClientApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @author: 章鑫
+ * @Email: zhangx511@chinaunicom.cn
+ * @Project_name：eurekaserver
+ * @Name: EurekaServerApplication
+ * @date: 2019-08-15 11:28
+ * @Description:
+ **/
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ClientApplication {
